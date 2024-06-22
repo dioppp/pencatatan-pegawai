a078740bb542f74199abc721fceb16275ca62f3c
Frisaranda Diouf Julio <frisaranda@gmail.com>
2024-06-23 00:47:47 +0700

    [STYLE] add layouts

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Simpeg</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('img/logos/logo-brown-4.svg') }}" />
+    <link rel="stylesheet" href="{{ asset('css/styles.min.css') }}" />
+</head>
+
+<body>
+    <!--  Body Wrapper -->
+    <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
+        data-sidebar-position="fixed" data-header-position="fixed">
+        @include('layouts.sidebar')
+        <!--  Main wrapper -->
+        <div class="body-wrapper">
+            @include('layouts.header')
+            @yield('content')
+        </div>
+    </div>
+    <script src="{{ asset('libs/jquery/dist/jquery.min.js') }}"></script>
+    <script src="{{ asset('libs/bootstrap/dist/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('js/sidebarmenu.js') }}"></script>
+    <script src="{{ asset('js/app.min.js') }}"></script>
+    <script src="{{ asset('libs/apexcharts/dist/apexcharts.min.js') }}"></script>
+    <script src="{{ asset('libs/simplebar/dist/simplebar.js') }}"></script>
+    <script src="{{ asset('js/dashboard.js') }}"></script>
+    @stack('scripts')
+</body>
+
+</html>
