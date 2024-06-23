b710f7fe73804f70f66eaa8f32a8bda8ce7cba62
Frisaranda Diouf Julio <frisaranda@gmail.com>
2024-06-23 20:18:15 +0700

    [FEAT] edit logout button

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,15 +19,14 @@
                             class="rounded-circle">
                     </a>
                     <div class="dropdown-menu dropdown-menu-end dropdown-menu-animate-up" aria-labelledby="drop2">
-                        <div class="message-body">
-                            <a href="javascript:void(0)" class="d-flex align-items-center gap-2 dropdown-item">
-                                <i class="ti ti-user fs-6"></i>
-                                <p class="mb-0 fs-3">My Profile</p>
-                            </a>
-                            <a href="{{ url('/') }}"
-                                class="btn btn-outline-primary mx-3 mt-2 d-block">Logout
-                            </a>
-                        </div>
+                        <form action="{{ route('logout') }}" method="POST" style="display: inline;">
+                            @csrf
+                            <button type="submit" class="btn btn-outline-primary mx-3 mt-2 d-block"
+                                style="width: 84%; padding: 10px;">
+                                <i class="ti ti-logout"></i>
+                                Logout
+                            </button>
+                        </form>
                     </div>
                 </li>
             </ul>
