633d651793859eb09eead0a7fe0074e0bc0936db
Frisaranda Diouf Julio <frisaranda@gmail.com>
2024-06-23 19:17:28 +0700

    [STYLE] edit dashboard and delete welcome view laravel template

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,55 +5,15 @@
         <div class="row">
             <div class="col-12">
                 <div class="card">
-                    <div class="card-header card-title fw-semibold mb-3">Dashboard</div>
+                    <div class="card-header card-title fw-semibold">
+                        <nav aria-label="breadcrumb">
+                            <ol class="breadcrumb p-3">
+                                <li class="breadcrumb-item active" aria-current="page">Dashboard</li>
+                            </ol>
+                        </nav>
+                    </div>
                     <div class="card-body">
-                        <div class="d-flex align-items-center justify-content-between position-relative mb-3">
-                            <a href="{{ url('/pegawai-add') }}" class="btn btn-primary">
-                                <i class="ti ti-plus"></i>
-                                <span class="ms-2">
-                                    Tambah Pegawai
-                                </span>
-                            </a>
-                        </div>
-
-                        {{-- Table --}}
-                        <div class="table-responsive">
-                            <table class="table text-nowrap mb-0 align-middle">
-                                <thead class="text-dark fs-4">
-                                    <tr>
-                                        <th class="border-bottom-0">
-                                            <h6 class="fw-semibold mb-0">No.</h6>
-                                        </th>
-                                        <th class="border-bottom-0">
-                                            <h6 class="fw-semibold mb-0">Nama</h6>
-                                        </th>
-                                        <th class="border-bottom-0">
-                                            <h6 class="fw-semibold mb-0">Tanggal Lahir</h6>
-                                        </th>
-                                        <th class="border-bottom-0">
-                                            <h6 class="fw-semibold mb-0">Jenis Kelamin</h6>
-                                        </th>
-                                        <th class="border-bottom-0">
-                                            <h6 class="fw-semibold mb-0">Departemen</h6>
-                                        </th>
-                                        <th class="border-bottom-0">
-                                            <h6 class="fw-semibold mb-0">Status Kepegawaian</h6>
-                                        </th>
-                                        <th class="border-bottom-0">
-                                            <h6 class="fw-semibold mb-0">Aksi</h6>
-                                        </th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td class="border-bottom-0 text-center" colspan="7">
-                                            <h6 class="fw-semibold mb-0">Tidak ada data</h6>
-                                        </td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
-                        {{-- End Table --}}
+                        <p>This is a sample page.</p>
                     </div>
                 </div>
             </div>
