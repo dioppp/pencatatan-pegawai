2221eb67beb8470336e8ad0f52c08f3c1f47f6fa
Frisaranda Diouf Julio <frisaranda@gmail.com>
2024-06-23 21:48:01 +0700

    [STYLE] change background image in login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -12,7 +12,8 @@
 <body>
     <!--  Body Wrapper -->
     <div class="page-wrapper" id="main-wrapper" data-layout="vertical" data-navbarbg="skin6" data-sidebartype="full"
-        data-sidebar-position="fixed" data-header-position="fixed">
+        data-sidebar-position="fixed" data-header-position="fixed"
+        style="background-image: url('{{ asset('img/bg-login.jpg') }}'); background-size: cover; background-position: center;">
         <div
             class="position-relative overflow-hidden radial-gradient min-vh-100 d-flex align-items-center justify-content-center">
             <div class="d-flex align-items-center justify-content-center w-100">
